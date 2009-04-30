99bdb79d5c38f9ed7b5045d611e225be5a635dcc
Cameron Walters <cameron.walters@gmail.com>
2009-04-30 16:04:53 -0700

    More refactoring to properly encapsulate code concerns.
    * uses Rack::Utils.escape for correct URL-encoding
    * parses response bodies as JSON

--- lib/cloudquery.rb
+++ lib/cloudquery.rb
@@ -2,12 +2,15 @@ require "rubygems"
 require "uri"
 require "digest/sha1"
 require "base64"
+require "rack/utils"
+require "curl"
+require "crack"
 
 module Cloudquery
   SCHEME = "https".freeze
   HOST = "api.xoopit.com".freeze
   PATH = "/v0".freeze
-  
+
   API_PATHS = {
     :account => "account",
   }.freeze
@@ -15,65 +18,66 @@ module Cloudquery
 
   SIGNING_METHOD = "SHA1"
   COOKIE_JAR = ".cookies.lwp"
-  
+
   class Request
     attr_accessor :method, :headers, :scheme, :host, :port, :path, :params, :body
-    
+
     def initialize(options={})
       @method = options[:method] || 'POST'
-      @headers = options[:headers] || {}
+      @headers = options[:headers] || []
       @scheme = options[:scheme] || SCHEME
       @host = options[:host] || HOST
       @port = options[:port] || URI::HTTPS::DEFAULT_PORT
       @path = options[:path] || PATH
       @params = options[:params] || {}
       @body = options[:body]
+      
+      @account = options[:account]
+      @secret = options[:secret]
     end
-    
-    def add_authentication_params(account)
-      @params['x_name'] = account
-      @params['x_time'] = Time.now.to_i_with_milliseconds
-      @params['x_nonce'] = Cloudquery::Crypto::Random.number
-      @params['x_method'] = SIGNING_METHOD
+
+    def request_uri(account=@account, secret=@secret)
+      uri = "#{@path}?#{query_str(signature_params(account))}"
+      uri = append_signature(uri, secret) if secret
+      uri
     end
-        
-    def request_uri
-      URI.build(:path => @path, :query => query_str).to_s
+
+    def url(account=@account, secret=@secret)
+      base_uri.merge(request_uri(account, secret)).to_s
     end
-    
-    def signed_request_uri(secret)
-      sign(request_uri, secret)
-    end
-    
-    def uri
-      base_uri.merge(request_uri).to_s
-    end
-    
-    def signed_uri(secret)
-      base_uri.merge(signed_request_uri).to_s
-    end
-    
-  private
+
+    private
     def append_signature(uri, secret)
-      signature = Crypto::Sha1.sign(secret, uri)
-      url_safe_signature = URI.escape(signature.tr('+/', '-_'), /=/)
-      "#{uri}&x_sig=#{url_safe_signature}"
+      sig = Crypto::Sha1.sign(secret, uri)
+      x_sig = Rack::Utils.build_query("x_sig" => sig.tr('+/', '-_'))
+      "#{uri}&#{x_sig}"
     end
-    
-    def query_str
-       @params.to_params
+
+    def signature_params(account=@account)
+      return {} unless account
+      {
+        'x_name' => account,
+        'x_time' => Time.now.to_i_with_milliseconds,
+        'x_nonce' => Cloudquery::Crypto::Random.number,
+        'x_method' => SIGNING_METHOD,
+      }
     end
-    
+
+    def query_str(additional_params={})
+      Rack::Utils.build_query(@params.dup.merge(additional_params))
+    end
+
     def base_uri
-      URI.build(:scheme => @scheme, :host => @host, :port => @port)
+      uri_class = if @scheme == 'https'
+        URI::HTTPS
+      else
+        URI::HTTP
+      end
+      uri_class.build(:scheme => @scheme, :host => @host, :port => @port)
     end
-    
-    def hash_to_params(hash)
-      hash.map { |k, v| URI.escape("#{k}=#{v}") }.join('&')
-      URI.escape(query_str, /@/)
-    end
+
   end
-  
+
   module Crypto
     module Random
       extend self
@@ -102,7 +106,7 @@ module Cloudquery
 
     end
   end
-  
+
   class Client
     attr_reader :account
     attr_writer :secret
@@ -112,21 +116,50 @@ module Cloudquery
       @secret = options[:secret]
       @secure = options[:secure] != false # must pass false for insecure
     end
-    
+
     def get_account
-      send_request Request.new(:path => build_path(API_PATHS[:account], account))
+      send_request build_request(:method => 'GET', :path => build_path(API_PATHS[:account], @account))
     end
-    
+
     private
-    def send_request(request)
-      p request
-    end
-    
     def build_path(*path_elements)
       path_elements.flatten.unshift(PATH).join('/')
     end
+    
+    def build_request(options={})
+      Request.new default_request_params.merge(options)
+    end
+    
+    def default_request_params
+      {
+        :account => @account,
+        :secret => @secret,
+        :scheme => @secure ? 'https' : 'http',
+      }
+    end
+    
+    def send_request(request)
+      execute_request(request.method, request.url, request.headers, request.body)
+    end
+
+    def execute_request(method, url, headers, body)
+      curl = Curl::Easy.new(url) do |c|
+        c.verbose = true
+        c.headers = headers
+      end
+      
+      case method
+      when 'GET'
+        curl.http_get
+      when 'POST'
+        curl.http_post(body)
+      end
+      
+      [curl.response_code, curl.header_str, Crack::JSON.parse(curl.body_str)]
+    end
   end
 end
+
 
 class Time
   def to_i_with_milliseconds
--- spec/cloudquery_spec.rb
+++ spec/cloudquery_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
+if ENV["TEST_REAL_HTTP"]
+  # Create a config.yml file containing the following:
+  # :account: <account name>
+  # :secret: <secret>
+  # then run the specs with TEST_REAL_HTTP=true
+  describe "CloudQuery account" do
+    before(:each) do
+      @config = YAML.load(File.read('config.yml'))
+      @client = Cloudquery::Client.new(@config)
+    end
+
+    it "gets your account information from the server" do
+      response_hash = @client.get_account.last
+      response_hash.should have_key("result")
+      response_hash["result"].should have_key("secret")
+      response_hash["result"]["secret"].should == @config[:secret]
+
+      response_hash["result"].should have_key("name")
+      response_hash["result"]["name"].should == @config[:account]
+
+      response_hash["result"].should have_key("preferences")
+    end
+  end
+end
+
 describe Cloudquery::Request do
   before(:each) do
     @valid_options = {
@@ -18,34 +43,73 @@ describe Cloudquery::Request do
     lambda { request }.should_not raise_error
   end
 
-  describe "add_authentication_params" do
-    before(:each) do
-      request.add_authentication_params('account')
-    end
-    
-    it "should add the x_name parameter with the account name" do
-      request.params.should have_key('x_name')
-      request.params['x_name'].should == 'account'
-    end
-    
-    it "should add the x_time parameter with the current milliseconds since epoch" do
-      request.params.should have_key('x_time')
-      request.params['x_time'].should be_close(Time.now.to_i_with_milliseconds, 100)
-    end
-    
-    it "should add the x_nonce parameter of the format \d+.\d+" do
-      request.params.should have_key('x_nonce')
-      request.params['x_nonce'].should match(/^\d+.\d+$/)
-    end
-    
-    it "should add the x_method parameter with the signing method name" do
-      request.params.should have_key('x_method')
-      request.params['x_method'].should == Cloudquery::SIGNING_METHOD
+
+  describe "append_signature" do
+    it "should append the signature as the x_sig parameter at the end of the query string" do
+      url = 'http://example.com/path?query=string'
+      signed_url = request.send(:append_signature, url, 'secret')
+      signed_url.should match(/^#{url.sub(/\?/, '\\?')}/)
+      signed_url.should match(/x_sig=[-\w]+(?:%3D)*$/)
     end
   end
 
-  describe "append_signature" do
-    it "should append the signature as the x_sig parameter at the end of the query string"
+  describe "signature_params" do
+    describe "without an account present" do
+      it "should return an empty hash" do
+        request.send(:signature_params).should == {}
+      end
+    end
+
+    describe "with an account present" do
+      before(:each) do
+        @params = request(:account => 'account').send(:signature_params)
+      end
+    
+      it "should return a hash with the x_name parameter with the account name" do
+        @params.should have_key('x_name')
+        @params['x_name'].should == 'account'
+      end
+    
+      it "should return a hash with the x_time parameter with the current milliseconds since epoch" do
+        @params.should have_key('x_time')
+        @params['x_time'].should be_close(Time.now.to_i_with_milliseconds, 100)
+      end
+    
+      it "should return a hash with the x_nonce parameter of the format \d+.\d+" do
+        @params.should have_key('x_nonce')
+        @params['x_nonce'].should match(/^\d+.\d+$/)
+      end
+    
+      it "should return a hash with the x_method parameter with the signing method name" do
+        @params.should have_key('x_method')
+        @params['x_method'].should == Cloudquery::SIGNING_METHOD
+      end
+    end
+  end
+  
+  describe "query_str" do
+    it "builds a query string from the request params" do
+      request(:params => {'these' => 'params'})
+      request.send(:query_str).should == 'these=params'
+    end
+    
+    it "url-encodes params with non alphanumeric characters (outside [ a-zA-Z0-9-._])" do
+      request(:params => {'weird' => 'values=here'})
+      request.send(:query_str).should == 'weird=values%3Dhere'
+    end
+    
+    it "returns an empty string when no params are present" do
+      request(:params => {}).send(:query_str) == ""
+    end
+  end
+  
+  describe "base_uri" do
+    it "returns an http url when the scheme is http" do
+      request(:scheme => 'http').send(:base_uri).should be_an_instance_of(URI::HTTP)
+    end
+    it "returns an https url when the scheme is https" do
+      request(:scheme => 'https').send(:base_uri).should be_an_instance_of(URI::HTTPS)
+    end
   end
 end
 
@@ -60,4 +124,19 @@ describe Cloudquery::Crypto::Random do
     end
   end
 end
-  
+
+describe Cloudquery::Crypto::Sha1 do
+  describe "sign" do
+    it "takes an abritrary number of tokens to encrypt" do
+      lambda { Cloudquery::Crypto::Sha1.sign }.should_not raise_error
+      lambda { Cloudquery::Crypto::Sha1.sign('a') }.should_not raise_error
+      lambda { Cloudquery::Crypto::Sha1.sign('a', 'b', 'c') }.should_not raise_error
+    end
+    
+    it "produces a base64 encoded SHA1 digest of the tokens without whitespace at the end" do
+      b64 = Base64.encode64(Digest::SHA1.digest('a')).chomp
+      Cloudquery::Crypto::Sha1.sign('a').should == b64
+    end
+  end
+end
+
